2f9cb9ed0fa85d07de4cbd6ad10369699b8a5645
brian <briansegs@gmail.com>
2022-10-05 09:50:35 -0400

    feat: Add function variables

--- breakout-01.py
+++ breakout-01.py
@@ -19,4 +19,31 @@ kucoin = ccxt.kucoinfutures({
     'password': c.kc_futures['API_PASSPHRASE'],
 })
 
+# Params:
 symbol = 'XBTUSDTM'
+index_pos = 0 # Change based on what asset
+
+# Time between trades
+pause_time = 10
+
+# For volume calc
+# vol_repeat * vol_time == TIME of volume collection
+vol_repeat = 11
+vol_time = 5
+
+pos_size = 10 # 125, 75,
+params = {'timeInForce': 'PostOnly'}
+target = 9
+max_loss = -8
+vol_decimal = .4
+
+
+# TODO: Add support/resist to functions
+# TODO: Pull in data: 1:50:21
+# TODO: Calculate support & resistance based on close
+# TODO: Calculate retest where we put orders
+# TODO: Run bot
+
+
+
+
